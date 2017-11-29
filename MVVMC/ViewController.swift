7e7ef9a2cb2771ec0818d471af92819bd1de424f
Dennis Merli <dennis.rodrigues@altran-ais.com.br>
2017-11-29 15:26:47 -0200

    Initial Commit

---
+++ MVVMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMC
+//
+//  Created by Dennis Merli on 11/29/17.
+//  Copyright © 2017 Dennis Merli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
